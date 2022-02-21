f45a4cdb983f3968569825454c8c52c02623049f
Aayush Agrawal <aag9711@gmail.com>
2022-02-21 17:31:46 +0530

    first commit

---
+++ SVM data.py
@@ -0,0 +1,10 @@
+Support Vector Regression:
+Support Vector Machine is a supervised learning algorithm which can be used for regression as well as classification problems. So if we use it for regression problems, then it is termed as Support Vector Regression.
+
+Support Vector Regression is a regression algorithm which works for continuous variables. Below are some keywords which are used in Support Vector Regression:
+
+Kernel: It is a function used to map a lower-dimensional data into higher dimensional data.
+Hyperplane: In general SVM, it is a separation line between two classes, but in SVR, it is a line which helps to predict the continuous variables and cover most of the datapoints.
+Boundary line: Boundary lines are the two lines apart from hyperplane, which creates a margin for datapoints.
+Support vectors: Support vectors are the datapoints which are nearest to the hyperplane and opposite class.
+In SVR, we always try to determine a hyperplane with a maximum margin, so that maximum number of datapoints are covered in that margin. The main goal of SVR is to consider the maximum datapoints within the boundary lines and the hyperplane (best-fit line) must contain a maximum number of datapoints. Consider the below image:
